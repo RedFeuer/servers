21e7d2c5f86f4360a2f0f126716973e409884560
имя_польname <_danilabespyatov@gmail.com>
2025-04-11 22:51:11 +0300

    ADDED: client

---
+++ client/source/client.hpp
@@ -0,0 +1,22 @@
+#ifndef CLIENT_HPP
+#define CLIENT_HPP
+
+#include "Socket.hpp"
+#include <string>
+
+namespace network {
+    class Client {
+    public:
+        Client(const std::string& serverHost, int serverPort);  // для коннекта к data_server
+        ~Client();
+
+        bool connectToServer();
+        void sendData(const std::string& data);
+        bool receiveAck();
+
+    private:
+        Socket clientSocket;
+        std::string serverHost;
+        int serverPort;
+    };
+}
