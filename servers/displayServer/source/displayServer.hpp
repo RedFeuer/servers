1a8a626be0294905f0b854a1548e5e7de9289eb3
имя_польname <_danilabespyatov@gmail.com>
2025-04-11 22:20:41 +0300

    ADDED: display server

---
+++ servers/displayServer/source/displayServer.hpp
@@ -0,0 +1,19 @@
+#ifndef DISPLAYSERVER_HPP
+#define DISPLAYSERVER_HPP
+
+#include "Server.hpp"
+
+namespace network {
+    class DisplayServer : public Server {
+    public:
+        ~DisplayServer();
+
+        void start(int port) override;
+        void stop() override;
+        void handleClient(Socket& client) override;
+
+    private:
+        Socket serverSocket;  // для приема данных от dataServer
+        bool isRunning = false;
+    };
+}
