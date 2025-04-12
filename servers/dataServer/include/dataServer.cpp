61692c541af7c011dc02187936fd129d32373a18
имя_польname <_danilabespyatov@gmail.com>
2025-04-12 16:34:43 +0300

    WORK IN PROGESS on handleClient

--- servers/dataServer/include/dataServer.cpp
+++ servers/dataServer/include/dataServer.cpp
@@ -17,6 +17,28 @@ namespace network {
         /*не смогли активировать прослушивание*/
         if (!serverSocket.listen()) {throw std::runtime_error("Listen failed");}
         isRunning = true;
+
+        /*Буквально по заданию
+         * После запуска, сервер обработки данных начинает ожидать подключения клиента.
+        Сервер обработки данных должен подключиться к серверу отображения результата.
+        */
+        while (isRunning) {
+            Socket cliendSocket;
+            if (serverSocket.accept(cliendSocket)) {
+                try { // так как handleClient выкидывает исключения
+                    handleClient(cliendSocket);
+                }
+                catch (...) { // все ошибки прокидываем дальше, чтобы потом обработать
+                    throw;
+                }
+            }
+            else {
+                /*Буквально по заданию
+                 * Если сервер не доступен или произошла ошибка подключения, то следует вывести ошибку на экран
+                 * и прекратить ожидание ввода данных.*/
+                throw std::runtime_error("Connection error"); // ВЫВОД ОШИБКИ ПРИ ОШИБКЕ ПОДКЛЮЧЕНИЯ
+            }
+        }
     }
 
     void DataServer::stop() {
@@ -28,6 +50,34 @@ namespace network {
     }
 
     void DataServer::handleClient(network::Socket &client) {
+        std::string request;
+        client.receive(request); // получаем данные от клиента в буфер request
 
+        /*подстрока "\r\n\r\n" - есть разделитель между заголовком и телом в HTTP POST*/
+        std::size_t body_pos = request.find("\r\n\r\n");
+        if (body_pos == std::string::npos) {
+            throw std::invalid_argument("Invalid HTTP request");
+        }
+
+        /*пропускаем "\r\n\r\n", чтобы попасть к телу сообщения*/
+        std::string jsonBody = request.substr(body_pos + 4);
+        /*парсим строку, чтобы получить объект nlohmann::json*/
+        nlohmann::json inputJson = nlohmann::json::parse(jsonBody);
+
+        if (!inputJson.contains("data")) { // чтобы не получить nullptr
+            /*в теле сообщения нет data(клиент не передал сообщение)*/
+            std::string errorResponse = "HTTP/1.1 400 Bad Request\r\n"
+                                        "Content-Type: application/json\r\n"
+                                        "Content-Length: 47\r\n\r\n"
+                                        "{\"error\":\"Missing required 'data' field\"}";
+            client.send(errorResponse);
+            return;
+        }
+        std::string data = inputJson["data"];
+
+        /*удаление из сообщения клиента дубликатов слов*/
+        std::string result = processData(data);
+
+        /*ТУТ ДОПИСАТЬ СВЯЗЬ С display_server и отправку туда данных*/
     }
 }
