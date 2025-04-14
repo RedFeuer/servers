4b3306cdc5dabd8e43bf13170fc68ad18e4a1b6c
имя_польname <_danilabespyatov@gmail.com>
2025-04-14 19:22:04 +0300

    ADDED: tests for dataServer

--- tests/dataServer.cpp
+++ tests/dataServer.cpp
@@ -13,3 +13,27 @@ TEST(DataServer, stop) {
     ASSERT_NO_FATAL_FAILURE(dataServer.stop());
     ASSERT_EQ(dataServer.getIsRunning(), false);
 }
+
+TEST(DataServer, getServerSocket) {
+    std::string displayHost = "127.0.0.1";
+    int displayPort = 9090;
+    network::DataServer dataServer(displayHost, displayPort);
+
+    ASSERT_EQ(typeid(dataServer.getServerSocket()), typeid(network::Socket));
+}
+
+TEST(DataServer, getDisplaySocket) {
+    std::string displayHost = "127.0.0.1";
+    int displayPort = 9090;
+    network::DataServer dataServer(displayHost, displayPort);
+
+    ASSERT_EQ(typeid(dataServer.getDisplaySocket()), typeid(network::Socket));
+}
+
+TEST(DataServer, getIsRunning) {
+    std::string displayHost = "127.0.0.1";
+    int displayPort = 9090;
+    network::DataServer dataServer(displayHost, displayPort);
+
+    ASSERT_EQ(typeid(dataServer.getIsRunning()), typeid(bool));
+}
